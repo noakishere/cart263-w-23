15c60dd35ca28f63eb3b3a093b238bea8ce01a28
kamyar <kamyarkarimi98@gmail.com>
2023-01-16 18:11:26 -0500

    Exercise 01 init

---
+++ Exercise_01/README.md
@@ -0,0 +1,5 @@
+# Exercise 01
+
+This project is used as a refresher on the coding concepts of CART-253.
+
+Using p5.js, this project replicates Tic Tac Toe.
---
+++ Exercise_01/js/script.js
@@ -0,0 +1,27 @@
+/**
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+/**
+Description of preload
+*/
+function preload() {}
+
+/**
+Description of setup
+*/
+function setup() {
+	createCanvas(500, 500);
+	background("red");
+}
+
+/**
+Description of draw()
+*/
+function draw() {}
