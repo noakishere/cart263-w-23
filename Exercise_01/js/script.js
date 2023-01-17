dacf5ecba8fa7abc4ba4da79887d6d7f588d284e
kamyar <kamyarkarimi98@gmail.com>
2023-01-16 19:24:18 -0500

    initial grid implemented

--- Exercise_01/js/script.js
+++ Exercise_01/js/script.js
@@ -1,12 +1,13 @@
 /**
-Title of Project
-Author Name
+Tic Tac Toe
+Author: Kamyar Karimi
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+This project uses p5.js to replicate Tic Tac Toe.
 */
 
 "use strict";
+
+let grids = [];
 
 /**
 Description of preload
@@ -18,10 +19,91 @@ Description of setup
 */
 function setup() {
 	createCanvas(500, 500);
-	background("red");
+	background("black");
+	drawBoard();
 }
 
 /**
-Description of draw()
-*/
-function draw() {}
+ Description of draw()
+ */
+function draw() {
+	background("black");
+	ruler();
+
+	grids.forEach((grid) => {
+		grid.display();
+	});
+}
+
+function CheckerGrid(x, y) {
+	this.x = x;
+	this.y = y;
+	this.col = color(255, 100);
+	this.middle = (x * 2 + y * 2) / 2;
+	this.shouldSign = false;
+	this.sign = "";
+
+	print(`Middle is: ${this.middle} and x ${this.x} and y ${this.y}`);
+	this.display = function () {
+		fill(this.col);
+		square(x, y, 55);
+
+		if (this.shouldSign) {
+			fill("white");
+			text(this.sign, this.x + 15, this.y + 27.5);
+		}
+	};
+
+	this.clicked = function (sign) {
+		var d = dist(mouseX, mouseY, this.x, this.y);
+		// var d = mouseX - this.middle;
+		if (d < 30 && !this.shouldSign) {
+			print(`${d}`);
+			this.col = color(255, 0, 200);
+			this.shouldSign = true;
+			this.sign = sign;
+		}
+	};
+}
+
+function drawBoard() {
+	// fill("white");
+
+	var xInit = 70;
+	var yInit = 100;
+
+	for (var y = 0; y < 3; y++) {
+		for (var x = 0; x < 3; x++) {
+			var newSquare = new CheckerGrid(xInit * x + 150, yInit + 50);
+			grids.push(newSquare);
+		}
+		yInit += 70;
+	}
+
+	// print(grids);
+}
+
+function mousePressed() {
+	for (var i = 0; i < grids.length; i++) {
+		grids[i].clicked("x");
+	}
+}
+
+/** PROD TOOLS */
+
+/*
+ * have a clearer idea of the points on the canvas
+ * only used for production, won't be used for the final result
+ */
+function ruler() {
+	for (let i = 1; i < 1080; i++) {
+		/* to have a clean look, as without this constraint
+		 * all points would be written on the screen.
+		 */
+		if (i % 50 === 0) {
+			fill("white");
+			text(i, i, 20);
+			text(i, 5, i);
+		}
+	}
+}
