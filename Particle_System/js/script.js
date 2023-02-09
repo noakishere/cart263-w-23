0cafbe30d95e2645213a03d23f6702fd49595d55
kamyar <kamyarkarimi98@gmail.com>
2023-02-08 19:14:52 -0500

    Circular fading particle added

--- Particle_System/js/script.js
+++ Particle_System/js/script.js
@@ -1,26 +1,92 @@
 /**
-Title of Project
-Author Name
+Title of Project: Feeling lonely in a crowd
+Author Kamyar Karimi
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+This project is an exercise on particle systems in P5js
 */
 
 "use strict";
 
+let fadingCircularParticles = [];
+
 /**
 Description of preload
 */
-function preload() {
-	createCanvas(500, 500);
-}
+function preload() {}
 
 /**
 Description of setup
 */
-function setup() {}
+function setup() {
+	createCanvas(750, 750);
+	background("black");
+
+	let p = new FadingCircularParticle();
+	fadingCircularParticles.push(p);
+}
 
 /**
 Description of draw()
 */
-function draw() {}
+function draw() {
+	background("black");
+
+	// for (let i = 0; i < 5; i++) {
+	// 	let p = new Particle();
+	// 	particles.push(p);
+	// }
+
+	for (let i = 0; i < fadingCircularParticles.length; i++) {
+		fadingCircularParticles[i].update();
+		fadingCircularParticles[i].show();
+		fadingCircularParticles[i].fear();
+
+		if (fadingCircularParticles[i].finished()) {
+			fadingCircularParticles.splice(i, 1);
+		}
+	}
+}
+
+function mousePressed() {
+	let p = new FadingCircularParticle();
+	fadingCircularParticles.push(p);
+}
+
+class FadingCircularParticle {
+	constructor() {
+		this.x = 300;
+		this.y = 380;
+		this.vx = random(-1, 1);
+		this.vy = random(-5, -1);
+		this.alpha = 255;
+		this.centerX = width / 2;
+		this.centerY = height / 2;
+		this.angle = 0;
+		this.speed = 0.05;
+		this.radius = 150;
+	}
+
+	show() {
+		noStroke();
+		fill(255, this.alpha);
+		ellipse(this.x, this.y, 16);
+	}
+
+	update() {
+		this.x = this.centerX + this.radius * cos(this.angle);
+		this.y = this.centerY + this.radius * sin(this.angle);
+		this.angle = this.angle + this.speed;
+		this.alpha -= 0.5;
+	}
+
+	finished() {
+		return this.alpha < 0;
+	}
+
+	fear() {
+		if (dist(this.x, this.y, mouseX, mouseY) <= 5) {
+			this.x += random(5, 10);
+			this.y += random(5, 10);
+		}
+	}
+}
